d58f1d35ac9d2332c32dfad945567b98c76c6ecc
Budzyn <black_witcher@ukr.net>
2017-08-08 19:22:09 +0300

    test

---
+++ GL/GL/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GL
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            FolderBrowserDialog db = new FolderBrowserDialog();
+            if(db.ShowDialog()==DialogResult.OK)
+            {
+                foreach(var path in Directory.GetFiles(db.SelectedPath))
+                {
+                    Console.WriteLine(path);
+                    Console.WriteLine(System.IO.Path.GetFileName(path));
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
